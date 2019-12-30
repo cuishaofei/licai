f827e6ed63cb3778b773c28c1327a9548d553482
cuisf <602591276@qq.com>
2019-12-30 12:00:43 +0800

    去掉长赢和U定投

--- src/test/java/com/feifei/licai/LicaiApplicationTests.java
+++ src/test/java/com/feifei/licai/LicaiApplicationTests.java
@@ -4,10 +4,9 @@ import com.feifei.licai.mapper.HistoryMapper;
 import com.feifei.licai.mapper.ProjectMapper;
 import com.feifei.licai.mapper.TotalMapper;
 import com.feifei.licai.service.ProjectService;
-import com.feifei.licai.util.Constants;
-import com.feifei.licai.util.LicaiType;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.ibatis.ognl.IntHashMap;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -19,10 +18,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+
 
 /**
  * @author cuishaofei
@@ -53,25 +51,8 @@ public class LicaiApplicationTests {
 
 	@Test
 	public void contextLoads() {
-//		projectMapper.getProjectList(Constants.orderCurrentMoney);
-//		projectMapper.getProjectList(Constants.orderAllProfit);
 
-		Map<String,Integer> map = new HashMap<>();
-		map.put("year1",2019);
-		map.put("year2",2018);
-		List<Map<String,Object>> list =  totalMapper.getJournalBetweenYear(map);
-		System.out.println(list);
 	}
 
-	@Test
-	public void getTotalCurrentMoneyByTypesTest(){
-		List<Integer> list = new ArrayList<>();
-		list.add(LicaiType.GUPIAO.getCode());
-		list.add(LicaiType.P2P.getCode());
-		list.add(LicaiType.ZHAIQUAN.getCode());
-		list.add(LicaiType.HUOBI.getCode());
-		double result = totalMapper.getTotalCurrentMoneyByTypes(list);
-		System.out.println(result);
-	}
 
 }
