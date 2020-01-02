506c7784ac25684b8e1382a1fdc15e5b9712e8cb
cuisf <602591276@qq.com>
2020-01-02 12:05:01 +0800

    修改跨年后每年的累计收益计算错误的问题。

--- src/main/java/com/feifei/licai/mapper/TotalMapper.java
+++ src/main/java/com/feifei/licai/mapper/TotalMapper.java
@@ -111,77 +111,79 @@ public interface TotalMapper {
      * 获取每年的总收益
      * @return
      */
-    @Select("SELECT " +
-            "    t6.yearStr, " +
-            "    SUM(t6.yearProfit) yearProfit " +
-            "FROM " +
-            "    ( " +
-            "        SELECT " +
-            "            t1.pid, " +
-            "            t1.lastUpdateTimeYear yearStr, " +
-            "            t1.currentMoney + ( " +
-            "                SELECT " +
-            "                    SUM(t2.optionMoney) " +
-            "                FROM " +
-            "                    lc_history t2 " +
-            "                WHERE " +
-            "                    t2.pid = t1.pid " +
-            "                AND t2.createTime <= t1.lastUpdateTime " +
-            "            ) - IFNULL( " +
-            "                ( " +
-            "                    SELECT " +
-            "                        t5.allYearProfit " +
-            "                    FROM " +
-            "                        ( " +
-            "                            SELECT " +
-            "                                t3.lastUpdateTimeYear, " +
-            "                                t3.currentMoney + ( " +
-            "                                    SELECT " +
-            "                                        SUM(t4.optionMoney) " +
-            "                                    FROM " +
-            "                                        lc_history t4 " +
-            "                                    WHERE " +
-            "                                        t4.pid = t3.pid " +
-            "                                    AND t4.createTime <= t3.lastUpdateTime " +
-            "                                ) allYearProfit " +
-            "                            FROM " +
-            "                                ( " +
-            "                                    SELECT " +
-            "                                        currentMoney, " +
-            "                                        pid, " +
-            "                                        lastUpdateTime, " +
-            "                                        DATE_FORMAT(lastUpdateTime, '%Y') lastUpdateTimeYear " +
-            "                                    FROM " +
-            "                                        lc_project_currentmoney " +
-            "                                    ORDER BY " +
-            "                                        lastUpdateTime DESC " +
-            "                                ) t3 " +
-            "                            GROUP BY " +
-            "                                t3.pid, " +
-            "                                DATE_FORMAT(t3.lastUpdateTime, '%Y') " +
-            "                        ) t5 " +
-            "                    WHERE " +
-            "                        t5.lastUpdateTimeYear = t1.lastUpdateTimeYear - 1 " +
-            "                ), " +
-            "                0 " +
-            "            ) yearProfit " +
-            "        FROM " +
-            "            ( " +
-            "                SELECT " +
-            "                    *, DATE_FORMAT(lastUpdateTime, '%Y') lastUpdateTimeYear " +
-            "                FROM " +
-            "                    lc_project_currentmoney " +
-            "                ORDER BY " +
-            "                    lastUpdateTime DESC " +
-            "            ) t1 " +
-            "        GROUP BY " +
-            "            t1.pid, " +
-            "            DATE_FORMAT(t1.lastUpdateTime, '%Y') " +
-            "    ) t6 " +
-            "GROUP BY " +
-            "    t6.yearStr " +
-            "ORDER BY " +
-            "    t6.yearStr DESC")
+    @Select("SELECT  " +
+            "	t6.yearStr,  " +
+            "	SUM(t6.yearProfit) yearProfit  " +
+            "FROM  " +
+            "	(  " +
+            "		SELECT  " +
+            "			t1.pid,  " +
+            "			t1.lastUpdateTimeYear yearStr,  " +
+            "			t1.currentMoney + (  " +
+            "				SELECT  " +
+            "					SUM(t2.optionMoney)  " +
+            "				FROM  " +
+            "					lc_history t2  " +
+            "				WHERE  " +
+            "					t2.pid = t1.pid  " +
+            "				AND t2.createTime <= t1.lastUpdateTime  " +
+            "			) - IFNULL(  " +
+            "				(  " +
+            "					SELECT  " +
+            "						t5.lastYearProfit  " +
+            "					FROM  " +
+            "						(  " +
+            "							SELECT  " +
+            "								t3.lastUpdateTimeYear,  " +
+            "								t3.currentMoney + (  " +
+            "									SELECT  " +
+            "										SUM(t4.optionMoney)  " +
+            "									FROM  " +
+            "										lc_history t4  " +
+            "									WHERE  " +
+            "										t4.pid = t3.pid  " +
+            "									AND t4.createTime <= t3.lastUpdateTime  " +
+            "								) lastYearProfit,  " +
+            "								t3.pid  " +
+            "							FROM  " +
+            "								(  " +
+            "									SELECT  " +
+            "										currentMoney,  " +
+            "										pid,  " +
+            "										lastUpdateTime,  " +
+            "										DATE_FORMAT(lastUpdateTime, '%Y') lastUpdateTimeYear  " +
+            "									FROM  " +
+            "										lc_project_currentmoney  " +
+            "									ORDER BY  " +
+            "										lastUpdateTime DESC  " +
+            "								) t3  " +
+            "							GROUP BY  " +
+            "								t3.pid,  " +
+            "								DATE_FORMAT(t3.lastUpdateTime, '%Y')  " +
+            "						) t5  " +
+            "					WHERE  " +
+            "						t5.lastUpdateTimeYear = t1.lastUpdateTimeYear - 1  " +
+            "					AND t5.pid = t1.pid  " +
+            "				),  " +
+            "				0  " +
+            "			) yearProfit  " +
+            "		FROM  " +
+            "			(  " +
+            "				SELECT  " +
+            "					*, DATE_FORMAT(lastUpdateTime, '%Y') lastUpdateTimeYear  " +
+            "				FROM  " +
+            "					lc_project_currentmoney  " +
+            "				ORDER BY  " +
+            "					lastUpdateTime DESC  " +
+            "			) t1  " +
+            "		GROUP BY  " +
+            "			t1.pid,  " +
+            "			DATE_FORMAT(t1.lastUpdateTime, '%Y')  " +
+            "	) t6  " +
+            "GROUP BY  " +
+            "	t6.yearStr  " +
+            "ORDER BY  " +
+            "	t6.yearStr DESC ")
     List<TotalVO>  getYearProfit();
 
     /**
